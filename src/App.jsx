cf441c216221539fe62312c1df7609dadc64a8e5
Arantxa Ordoyo <44567127+arianxa@users.noreply.github.com>
2026-01-30 08:52:10 +0000

    fallos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import { Aboutme } from "./components/Aboutme";
 import { Skills } from "./components/Skills";
-import { Projects } from "./components/projects";
+import { Projects } from "./components/Projects";
 import { Hero } from "./components/Hero";
 import Contact from "./components/Contact";
 
